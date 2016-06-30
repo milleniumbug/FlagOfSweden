812ab72610367bc03c93fde8e368058d12a40468
Johan Larsson <johan_gavle1@hotmail.com>
2016-06-30 21:40:49 +0200

    Sample

---
+++ WpfApplication1/FlagOfSwedenView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace WpfApplication1
+{
+    using System.Windows.Controls;
+
+    public partial class FlagOfSwedenView : UserControl
+    {
+        public static readonly DependencyProperty NestedProperty = DependencyProperty.Register(
+            "Nested",
+            typeof(FlagOfSwedenView), 
+            typeof(FlagOfSwedenView), 
+            new PropertyMetadata(default(object)));
+
+        public FlagOfSwedenView()
+        {
+            InitializeComponent();
+        }
+
+        public FlagOfSwedenView Nested
+        {
+            get { return (FlagOfSwedenView)GetValue(NestedProperty); }
+            set { SetValue(NestedProperty, value); }
+        }
+    }
+}
